85b98a68e627fb1374948238b000649908914e1b
tarunUpadhyay <tarunjataura@gmail.com>
2020-01-28 13:13:20 +0530

    Initial Commit

---
+++ Tarun_wiproAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tarun_wiproAssignment
+//
+//  Created by Tarun Upadhyay on 28/01/20.
+//  Copyright © 2020 Tarun Upadhyay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
